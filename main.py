76d960bff9dc432e12aa667c4b3d74ade464c666
Marina Sintsova <56174113+sintsovamy@users.noreply.github.com>
2022-05-31 14:26:55 +0000

    smarter version

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ rot = int(input())
 for c in text:
   if c not in "!#$%&*+-=?@^_,." and c != " ":
     if c >= "А" and c <= "Я":
-      c = chr(ord("А") + (ord(c) + rot) % ord ("А") % 32)
+      c = chr((ord(c) + rot - ord("А")) % 32 + ord("А"))
     elif c >= "а" and c <= "я":
-      c = chr(ord("а") + (ord(c) + rot) % ord ("а") % 32)
+      c = chr((ord(c) + rot - ord("а")) % 32 + ord("а"))
     elif c >= "A" and c <= "Z":
-      c = chr(ord("A") + (ord(c) + rot) % ord ("A") % 26)
+      c = chr((ord(c) + rot - ord("A")) % 26 + ord("A"))
     elif c >= "a" and c <= "z":
-      c = chr(ord("a") + (ord(c) + rot) % ord ("a") % 26)
+      c = chr((ord(c) + rot - ord("a")) % 26 + ord("a"))
   cyphertext +=c
 print(cyphertext)
+print(chr(ord("а") + ((ord("ж") + rot) % ord ("а")) % 32))
+print((ord("ж") + rot) % ord ("а") % 32)
